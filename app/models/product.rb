1b5b38b53013e40c851816380ac609fd2b8e8357
Chandresh Pant <cp@cpant.in>
2013-06-15 20:31:03 +0530

    basic inventory

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,15 +1,25 @@
 class Product < ActiveRecord::Base
-  attr_accessible :description, :model_no, :name, :price
+  attr_accessible :description, :model_no, :name, :price, :inventory
 
 
-  ## class method
+  ## class methods
 
   def self.home_page_products
     order.sample(9)
   end
 
+  def self.available
+    where("inventory is not null and inventory > ?", 0)
+  end
+
+  # instance methods
+
   def related_products
     Product.order.sample(3) # we do not know yet
+  end
+
+  def in_stock?
+    inventory.to_i > 0
   end
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130615014652) do
+ActiveRecord::Schema.define(:version => 20130615145226) do
 
   create_table "products", :force => true do |t|
     t.string   "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(:version => 20130615014652) do
     t.string   "model_no"
     t.datetime "created_at",                                :null => false
     t.datetime "updated_at",                                :null => false
+    t.integer  "inventory"
   end
 
 end
